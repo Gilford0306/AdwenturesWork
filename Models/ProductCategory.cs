c923fd108464942ce8c5130640951c318ab1c28b
Gilford0306 <Gilford0306@gmail.com>
2023-06-17 13:34:46 +0300

    Add project files.

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AdwenturesWork.Models;
+
+/// <summary>
+/// High-level product categorization.
+/// </summary>
+public partial class ProductCategory
+{
+    /// <summary>
+    /// Primary key for ProductCategory records.
+    /// </summary>
+    public int ProductCategoryId { get; set; }
+
+    /// <summary>
+    /// Category description.
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// ROWGUIDCOL number uniquely identifying the record. Used to support a merge replication sample.
+    /// </summary>
+    public Guid Rowguid { get; set; }
+
+    /// <summary>
+    /// Date and time the record was last updated.
+    /// </summary>
+    public DateTime ModifiedDate { get; set; }
+
+
+    public ProductCategory()
+    {
+    }
+    public virtual ICollection<ProductSubcategory> ProductSubcategories { get; set; } = new List<ProductSubcategory>();
+    public override string ToString()
+    {
+        return $"{ProductCategoryId}) {Name} - {ModifiedDate}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using AdwenturesWork;
+using AdwenturesWork.Models;
+
+using (AdventureWorks2022Context context = new AdventureWorks2022Context())
+{
+
+    foreach (ProductCategory item in context.ProductCategories)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine("============================");
+    //add
+    context.ProductCategories.Add(new ProductCategory() { Name = "Text 3", ModifiedDate = DateTime.Now });
+    context.SaveChanges();
+
+    foreach (ProductCategory item in context.ProductCategories)
+    {
+        Console.WriteLine(item);
+    }
+
+    //remove
+    int idForRemove = 9;
+    context.ProductCategories.Remove(context.ProductCategories.FirstOrDefault(x => x.ProductCategoryId.Equals(idForRemove)));
+    context.SaveChanges();
+    Console.WriteLine("============================");
+    foreach (ProductCategory item in context.ProductCategories)
+    {
+        Console.WriteLine(item);
+    }
+
+    //update
+    int idForUpdate = 10;
+    var oldData = context.ProductCategories.FirstOrDefault(x => x.ProductCategoryId == idForUpdate);
+    oldData.Name = "New Text";
+    context.ProductCategories.Update(oldData);
+    context.SaveChanges();
+    Console.WriteLine("============================");
+    foreach (ProductCategory item in context.ProductCategories)
+    {
+        Console.WriteLine(item);
+    }
+
+}
